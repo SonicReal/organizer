1f0fea3496c6cfd2b0c7b7947f05713827a97994
Sonic The Hedgehog <blueblur@bk.ru>
2016-09-15 17:33:54 +0500

    !!!!

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('hello world');
-console.log("How are you?");
-console.log("baba");
-console.log("kaka2");
+console.log('hello world');!!!!!!!
+console.log("How are you?");!!!!!!!!!
+console.log("baba");!!!!!!!!!
+console.log("kaka2");!!!!!!!!!!
